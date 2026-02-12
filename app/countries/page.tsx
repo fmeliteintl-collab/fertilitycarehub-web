d1098f4f6e9a729271ed0f5da8fb272b78549d91
fmeliteintl-collab <fmeliteintl@gmail.com>
2026-02-12 02:22:38 +0000

    Fix countries page module error

---
+++ app/countries/page.tsx
@@ -0,0 +1,8 @@
+export default function CountriesPage() {
+  return (
+    <main style={{ padding: "80px 20px", textAlign: "center" }}>
+      <h1>Global Fertility Destinations</h1>
+      <p>Explore structured strategic fertility intelligence by country.</p>
+    </main>
+  );
+}
