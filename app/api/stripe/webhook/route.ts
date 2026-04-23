a127f7fed2f7d841b7c2287475f04a5ed16a15d3
fmeliteintl-collab <fmeliteintl@gmail.com>
2026-04-23 21:06:56 +0000

    Set Stripe webhook route to edge runtime

--- app/api/stripe/webhook/route.ts
+++ app/api/stripe/webhook/route.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from "next/server";
 import Stripe from "stripe";
 import { headers } from "next/headers";
-
+export const runtime = "edge";
 export async function POST(req: Request) {
   const stripeSecretKey = process.env.STRIPE_SECRET_KEY;
   const stripeWebhookSecret = process.env.STRIPE_WEBHOOK_SECRET;
